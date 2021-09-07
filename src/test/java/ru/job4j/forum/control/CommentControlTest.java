188751b0d15a5b596a4ba2fa88e5e89f9186864c
SavvaMey <savva28031997@gmail.com>
2021-09-07 18:13:31 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/CommentControlTest.java
+++ src/test/java/ru/job4j/forum/control/CommentControlTest.java
@@ -2,6 +2,7 @@ package ru.job4j.forum.control;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,16 +10,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Comment;
 import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.CommentService;
 import ru.job4j.forum.service.PostService;
 
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -28,50 +29,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
-public class PostControlTest {
-
+public class CommentControlTest {
+    @MockBean
+    private CommentService commentService;
     @MockBean
     private PostService posts;
-
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     @WithMockUser
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/edit?id=3"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("forum/edit"));
-    }
-
-    @Test
-    @WithMockUser
-    public void shouldReturnDefaultMessageAnotherMethod() throws Exception {
-        this.mockMvc.perform(get("/create"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("forum/create"));
-    }
-
-    @Test
-    @WithMockUser
-    public void shouldReturnDefaultMessageAnotherSecMethod() throws Exception {
-        this.mockMvc.perform(get("/post?id=3"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("forum/post"));
-    }
-
-    @Test
-    @WithMockUser
-    public void MethodSaveForPost() throws Exception {
-        this.mockMvc.perform(post("/save")
-                .param("name","Куплю ладу-грант. Дорого."))
+    public void CommentServiceMethodPost() throws Exception {
+        Post post = Post.of("Куплю ладу-грант. Дорого.", "бюджет 100к");
+        post.setId(1);
+        Mockito.when(posts.findPostById(Mockito.anyInt())).thenReturn(Optional.of(post).get());
+        this.mockMvc.perform(post("/postComment?idPost=1")
+                .param("message","koment"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection());
-        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
-        verify(posts).createOrUpdatePost(argument.capture());
-        assertThat(argument.getValue().getName(), is("Куплю ладу-грант. Дорого."));
+        ArgumentCaptor<Comment> argument = ArgumentCaptor.forClass(Comment.class);
+        verify(commentService).postComment(argument.capture(), 1);
+        assertThat(argument.getValue().getMessage(), is("koment"));
     }
 }
